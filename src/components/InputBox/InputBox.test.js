5686c575978a5736dfac2516290dafa2746444c6
Vinícius Marrara Banaco <vbanaco@masti.com.br>
2019-09-23 10:07:49 -0300

    refactor: muda o escopo da variável `component`
    Troca o escopo da variável `component` no arquivo de teste do
    `InputBox`, para que cada método possua a sua própria e evite problemas
    futuros.

--- src/components/InputBox/InputBox.test.js
+++ src/components/InputBox/InputBox.test.js
@@ -6,15 +6,17 @@ import InputBox from './InputBox';
 const clickFn = jest.fn();
 
 describe('InputBox', () => {
-  const component = shallow(<InputBox callback={clickFn} />);
-
   // Render properly with props
   it("should render properly with 'callback' prop", () => {
+    const component = shallow(<InputBox callback={clickFn} />);
+
     expect(component).toMatchSnapshot();
   });
 
   // Call the callback
   it('should call the callback function with the value of the input', () => {
+    const component = shallow(<InputBox callback={clickFn} />);
+
     component.find('input').simulate('change', { target: { value: 'Vinícius' } });
     component.find('button').simulate('click');
 
