5e4d537501f86f2bf19935cda5cc79c92e913d7e
Vinícius Marrara Banaco <vbanaco@masti.com.br>
2019-09-23 09:56:38 -0300

    refactor: converte `Hello` em componente funcional

--- src/Hello/Hello.jsx
+++ src/Hello/Hello.jsx
@@ -1,56 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import InputBox from '../components/InputBox/InputBox';
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
+function Hello({ name }) {
+  const [userName, setUserName] = useState('');
+  const [visible, setVisible] = useState(true);
 
-    this.state = {
-      name: '',
-      visible: true,
-    };
+  useEffect(() => {
+    setUserName(name);
+  }, [name]);
+
+  function toggleText() {
+    setVisible(!visible);
   }
 
-  componentDidMount() {
-    const { name } = this.props;
-    this.setState({ name });
+  function changeTitle(text) {
+    setUserName(text);
   }
 
-  toggleText = () => {
-    const { visible } = this.state;
-    this.setState({
-      visible: !visible,
-    });
-  }
-
-  changeTitle = (text) => {
-    this.setState({
-      name: text,
-    });
-  }
-
-  renderTitle = () => {
-    const { visible, name } = this.state;
-
+  function renderTitle() {
     if (visible) {
-      return <h1>{name}</h1>;
+      return <h1>{userName || name}</h1>;
     }
 
     return null;
   }
 
-  render() {
-    return (
-      <>
-        {this.renderTitle()}
-        <button type="button" onClick={this.toggleText}>Toggle text</button>
+  return (
+    <>
+      {renderTitle()}
+      <button type="button" onClick={toggleText}>Toggle text</button>
 
-        <InputBox style={{ marginTop: '40px' }} callback={this.changeTitle} />
-      </>
-    );
-  }
+      <InputBox style={{ marginTop: '40px' }} callback={changeTitle} />
+    </>
+  );
 }
 
 export default Hello;
